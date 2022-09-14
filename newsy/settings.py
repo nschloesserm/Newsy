ead8586fdd7c3573dde8fbea2bd88dcf99bfb0be
nschloesserm <nicholasmckennaart@gmail.com>
2022-09-14 16:45:48 +0000

    deployment commit

--- newsy/settings.py
+++ newsy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://newsy2022.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['newsy2022.herokuapp.com', 'localhost']
 
 
 # Application definition
